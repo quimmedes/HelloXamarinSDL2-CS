e376a2b0adb90a12a135f6604bcacdc6ff40a239
kimede <quimmedes@gmail.com>
2024-09-09 20:35:08 -0300

    It works finally

--- MainActivity.cs
+++ MainActivity.cs
@@ -1,9 +1,11 @@
 using Android.Nfc;
 using Android.OS;
 using Android.Util;
+using Android.Views;
 using Java.Lang;
 using Org.Libsdl.App;
 using SDL2;
+using System.Runtime.InteropServices;
 using static SDL2.SDL;
 using static SDL2.SDL_image;
 
@@ -11,137 +13,50 @@ using static SDL2.SDL_image;
 namespace HelloXamarinSDL_CS
 {
 
+
     [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : SDLActivity
     {
+        public static MainActivity Instance { get ; protected set; }
+        public static bool SDL2DCS_Fullscreen = true;
         static nint window;
         static nint renderer;
-        protected override void OnCreate(Bundle? savedInstanceState)
+
+        [DllImport("main")]
+        static extern void SetMain(Main main);
+
+
+        public static void Init()
         {
-            //base.OnCreate(savedInstanceState);
-
-            Log.Verbose("Device: ", Build.Device);
-            Log.Verbose("Model: " , Build.Model);
-            Log.Verbose("","OnCreate()");
-            base.OnCreate(savedInstanceState);
-
-            try
-            {
-
-                Java.Lang.Thread.CurrentThread().Name = "SDLActivity";
-
-            }
-            catch (System.Exception e)
-            {
-                Log.Error( "modify thread properties failed: " , e.Message);
-            }
-
-            // Load shared libraries
-            string errorMsgBrokenLib = "";
 
 
-            try
-            {
-               foreach(string lib in GetLibraries())
-                {
-                    Org.Libsdl.App.SDL.LoadLibrary(lib);
-                }
-                MBrokenLibraries = false; /* success */
-            }
-            catch (UnsatisfiedLinkError e)
-            {
-                e.PrintStackTrace();
-                MBrokenLibraries = true;
-                errorMsgBrokenLib = e.Message;
-            }
-            catch (System.Exception e)
-            {
-
-                Log.Error("", e.Message);
-                MBrokenLibraries = true;
-                errorMsgBrokenLib = e.Message;
-            }
-
-           
-
-            if (MBrokenLibraries)
-            {
-                MSingleton = this;
-                AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
-                dlgAlert.SetMessage("An error occurred while trying to start the application. Please try again and/or reinstall."
-                      + Java.Lang.JavaSystem.GetProperty("line.separator")
-                      + Java.Lang.JavaSystem.GetProperty("line.separator")
-                      + "Error: " + errorMsgBrokenLib);
-                dlgAlert.SetTitle("SDL Error");
-
-                dlgAlert.SetPositiveButton("Exit", (sender, args) =>
-                {
-                    MSingleton.Finish();
-                });
-
-                dlgAlert.SetCancelable(false);
-                dlgAlert.Create().Show();
-
-                return;
-            }
-
-
-            MSingleton = this;
-            Org.Libsdl.App.SDL.Context = this;
-
-          //  MClipboardHandler = new SDLClipboardHandler(this);
-           // MHIDDeviceManager = new SDLHIDDeviceManager();
-
-            Org.Libsdl.App.SDL.SetupJNI();
+            SDL_Init(SDL_INIT_EVERYTHING);
+            IMG_Init(IMG_InitFlags.IMG_INIT_PNG | IMG_InitFlags.IMG_INIT_JPG);
             
-            MSurface = CreateSDLSurface(this);
-            MLayout = new RelativeLayout(this);
-            MLayout.AddView(MSurface);
-            MCurrentOrientation = SDLActivity.MCurrentOrientation;
-            OnNativeOrientationChanged(MCurrentOrientation);
 
-            SetContentView(MLayout);
-            SetWindowStyle(false);
-            this.Window.DecorView.SetOnSystemUiVisibilityChangeListener(this);
+            DisplayMetrics dm = new DisplayMetrics();
+            Instance.WindowManager.DefaultDisplay.GetMetrics(dm);
 
-
-            // Set our view from the "main" layout resource
-            //   SetContentView(Resource.Layout.activity_main);
-
-            if (SDL_Init(SDL_INIT_EVERYTHING)<0)
-            {
-                Toast.MakeText(this, "SDL_Init failed", ToastLength.Long).Show();
-            }
-            else
-            {
-                Toast.MakeText(this, "SDL_Init success "+ NativeGetVersion(), ToastLength.Long).Show();
-            }
-
-
-
-            if (IMG_Init(IMG_InitFlags.IMG_INIT_PNG | IMG_InitFlags.IMG_INIT_JPG) < 0)
-            {
-                Toast.MakeText(this, "SDL_image failed", ToastLength.Long).Show();
-            }
-            else
-            {
-                Toast.MakeText(this, "SDL_image success " , ToastLength.Long).Show();
-            }
-
-
-             window = SDL_CreateWindow("An SDL2 window!", SDL_WINDOWPOS_UNDEFINED, 0, 640, 480, SDL_WindowFlags.SDL_WINDOW_OPENGL);
+            window = SDL_CreateWindow(
+                "HEY, LISTEN!",
+                SDL_WINDOWPOS_CENTERED,
+                SDL_WINDOWPOS_CENTERED,
+                dm.WidthPixels,
+                dm.HeightPixels,
+                SDL_WindowFlags.SDL_WINDOW_OPENGL |
+                SDL_WindowFlags.SDL_WINDOW_INPUT_FOCUS |
+                SDL_WindowFlags.SDL_WINDOW_MOUSE_FOCUS | 
+                SDL_WindowFlags.SDL_WINDOW_FULLSCREEN
+            );
 
              renderer = SDL_CreateRenderer(window, -1, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
 
-            nint texture = IMG_LoadTexture(renderer, Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "player.png")); 
+            nint texture = IMG_LoadTexture(renderer, Path.Combine("file:///android_asset", "player.png")); 
 
-            if(texture > 0)
-                Toast.MakeText(this, "Texture loaded", ToastLength.Long).Show();
-
-
+           
 
             int i = 0;
-            while(i<300)
+            while(i<600)
             {
                 SDL_Event e;
                 while (SDL_PollEvent(out e) != 0)
@@ -154,7 +69,7 @@ namespace HelloXamarinSDL_CS
                 i++;
 
                 SDL_RenderClear(renderer);
-                SDL_SetRenderDrawColor(renderer, 0, 128, 0, 255);
+                SDL_SetRenderDrawColor(renderer, (byte) (i % 255), 128, 0, 255);
 
 
                 blit(texture, 100, 100);
@@ -172,10 +87,40 @@ namespace HelloXamarinSDL_CS
 
 
         }
+        
+        
+
+        public override void LoadLibraries()
+        {
+            base.LoadLibraries();
+            Instance = this;
+            Bootstrap.SetMain(Init);
+            // Bootstrap.SetupMain();
+        }
+
+        public override void OnWindowFocusChanged(bool hasFocus)
+        {
+            base.OnWindowFocusChanged(hasFocus);
+            if (hasFocus && SDL2DCS_Fullscreen)
+            {
+                Window.DecorView.SystemUiFlags = (SystemUiFlags)(
+                    SystemUiFlags.LayoutStable |
+                    SystemUiFlags.LayoutHideNavigation |
+                    SystemUiFlags.LayoutFullscreen |
+                    SystemUiFlags.HideNavigation |
+                    SystemUiFlags.Fullscreen |
+                    SystemUiFlags.ImmersiveSticky
+                );
+            }
+        }
+
+        
+        
 
         protected override string[]? GetLibraries()
         {
-            return new string[] { "SDL2", "main","SDL2_image"  };
+
+            return new string[] { "SDL2", "SDL2_image", "main"  };
         }
 
         public static void blit(IntPtr texutre, float x, float y)
